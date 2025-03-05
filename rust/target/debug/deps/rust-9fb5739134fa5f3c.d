de2d4e1655c81dd0401e92281d5a3b0b2c438a7d
ghosthair <141095211+ghosthair@users.noreply.github.com>
2025-03-04 19:27:22 -0700

    Trying out rust

---
+++ rust/target/debug/deps/rust-9fb5739134fa5f3c.d
@@ -0,0 +1,5 @@
+/Users/david/Desktop/gitrepos/random_practice/rust/target/debug/deps/rust-9fb5739134fa5f3c: src/main.rs
+
+/Users/david/Desktop/gitrepos/random_practice/rust/target/debug/deps/rust-9fb5739134fa5f3c.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/Users/david/Desktop/gitrepos/random_practice/rust/target/debug/rust: /Users/david/Desktop/gitrepos/random_practice/rust/src/main.rs
